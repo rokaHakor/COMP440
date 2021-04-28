7a6e8469a830ad6b2bda9fc20f4d9d4220de3e46
BSCalderon <ballardo.calderon.764@my.csun.edu>
2021-04-27 20:57:11 -0700

    Added listeners for LoginView and cartCheckout.

--- src/com/database/project/cartCheckout.java
+++ src/com/database/project/cartCheckout.java
@@ -1,10 +1,52 @@
 package com.database.project;
 
 import javax.swing.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
-public class cartCheckout {
+public class cartCheckout extends JFrame{
     private JTextField couponCode;
-    private JTextField address;
+    private JTextField addressText;
     private JTextField bankAccountNumber;
     private JButton submitButton;
+    private String coupon;
+    private String address;
+    private String bankAccount;
+
+    public cartCheckout(JFrame frame){
+
+        couponCode.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                coupon = couponCode.getText();
+            }
+        });
+        addressText.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                address = addressText.getText();
+            }
+        });
+        bankAccountNumber.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                bankAccount = bankAccountNumber.getText();
+            }
+
+        });
+    }
 }
