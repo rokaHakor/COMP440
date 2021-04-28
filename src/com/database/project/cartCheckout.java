5e49dca2be7c886fe8a63a4b96ba4cb7810d0343
BSCalderon <ballardo.calderon.764@my.csun.edu>
2021-04-27 17:43:30 -0700

    Added basic cart checkout view

---
+++ src/com/database/project/cartCheckout.java
@@ -0,0 +1,10 @@
+package com.database.project;
+
+import javax.swing.*;
+
+public class cartCheckout {
+    private JTextField couponCode;
+    private JTextField address;
+    private JTextField bankAccountNumber;
+    private JButton submitButton;
+}
