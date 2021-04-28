6f49ab1e3741e42a3105f5ecbab5edb2db1f6569
BSCalderon <ballardo.calderon.764@my.csun.edu>
2021-04-27 20:57:11 -0700

    Added listeners for LoginView and cartCheckout.

--- src/com/database/project/LoginView.java
+++ src/com/database/project/LoginView.java
@@ -1,6 +1,10 @@
 package com.database.project;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class LoginView {
     public JPanel panel1;
@@ -8,8 +12,34 @@ public class LoginView {
     private JButton loginButton;
     private JPasswordField passwordField;
     private JTextField usernameField;
+    private String username;
+    private String password;
 
     public LoginView(JFrame frame){
 
+        usernameField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+            }
+            public void focusLost(FocusEvent e) {
+                username = usernameField.getText();
+            }
+        });
+        passwordField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+            }
+            public void focusLost(FocusEvent e){
+                password = String.valueOf(passwordField.getPassword());
+            }
+        });
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(username + " " + password);
+            }
+        });
     }
 }
